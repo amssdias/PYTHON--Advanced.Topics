dbbd8439344b5aae3131cd950ee448d108d0e768
André Dias <andremssdias@hotmail.com>
2021-02-24 11:00:08 +0100

    Receiving thru yield

--- Asynchronous Dev/async_await.py
+++ Asynchronous Dev/async_await.py
@@ -1,8 +1,10 @@
 from collections import deque
+from types import coroutine
 
 friends = deque(('Rolf', 'Jose', 'Charlie', 'Jen', 'Anna'))
 
 
+@coroutine
 def friend_upper():
     while friends:
         friend = friends.popleft().upper()
@@ -10,16 +12,18 @@ def friend_upper():
         print(f'{greeting} {friend}')
 
 
-def greet(g):
-    yield from g
-    # g.send(None)
-    # while True:
-    #     greeting = yield
-    #     g.send(greeting)
+async def greet(g):
+    print('Starting...')
+    await g
+    print('Ending...')
 
 
 greeter = greet(friend_upper())
 greeter.send(None)
 greeter.send('Hello')
-print('Hello, world! Multitasking...')
-greeter.send('How are you,')
+
+greeting = input('Enter a greeting: ')
+greeter.send(greeting)
+
+greeting = input('Enter a greeting: ')
+greeter.send(greeting)
